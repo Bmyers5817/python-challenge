d26a2b83d50f42aff0e1d33a36dc632fe54781bd
Beth Myers <bmyers5817@gmail.com>
2021-01-12 10:34:00 -0500

    Pushing final python code and output results for python challenge homework 3

--- Instructions/PyPoll/main.py
+++ Instructions/PyPoll/main.py
@@ -8,69 +8,61 @@ import csv
 # Read and collect path of data source from the Resources folder
 election_csv = os.path.join("Resources", "election_data.csv")
 
-# Capture list of candidates with votes casted & unique canidate
-candidate = []
-uniq_cand = []
-
-# Counter for total number of votes
-vote_count = 0
-
-# # Variable to capture votes per candidate 
-# cand_vote = []
-
-# Variable to capture percent of total votes per candidate
-prct_votes = []
+# Capture list of candidates with votes cast and count of votes
+candidates = []
+vote_count = []
 
 # Open and set reader for csv file
 with open(election_csv, newline="") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csv_reader)
 
- # Creation of for statement to read and collect data
+# Creation of for statement to read and collect data
     for row in csv_reader:
+        # Check and capture unique candidates 
+        if row[2] not in candidates:
+            candidates.append(row[2])
+            # Append vote count by candidate
+            vote_count.append(1)
+        else:
+            # Use of voteindex to capture votes upon candidate change
+            # * vote_count[voteindex] was not working without the +=, which was researched with various sites (CodeAcademy/SlackOverflow)
+            voteindex = candidates.index(row[2])
+            vote_count[voteindex] += 1
+
+    # Count for total number of votes
+    total_votes = sum(vote_count)
+
+    # Capture percent of total votes per candidate
+    votepercent = [round(vote_count[i]/total_votes*100,4) for i in range(0,len(vote_count))]
+
     
-        # Use vote_count to count the number of votes cast
-        vote_count = vote_count + 1
-
-        # Set candidate list
-        candidate.append(row[2])
-
-        # Set unique candidates, x to each unique candidate, y to number of votes / candidate, and z to percentage of votes / candidate
-    for x in set(candidate):
-        uniq_cand.append(x)
-        y = candidate.count(x)
-        vote_count.append(y)
-        z = (y/vote_count)*100, 3
-        #z = round(percentage) - see if 43 works
-        prct_votes.append(z)
-        # set format for percentage data   
-        prct_votes = "%.3f%%" % z
-
-  # Find winner of votes cast
-    cand_winner = max(vote_count)
-    winner = uniq_cand[vote_count.index(cand_winner)]
-
 # Print results table to terminal 
 
+print("-----------------------------------")
 print("Election Results")
 print("-----------------------------------")
-print(" Total Votes:  " + str(vote_count))
+print(" Total Votes:  " + str(total_votes))
 print("-----------------------------------")
-for i in range(len(candidate)):
-    print(candidate[i] + ":" + str(prct_votes[i]) +"% (" + str(vote_count[i])+")")
+
+for i in range(len(candidates)):
+    print(f'{candidates[i]}:  {votepercent[i]}% ({vote_count[i]})')
+
 print("-----------------------------------")    
-print("The Winner is:  " + winner)
+print(f"The Winner is:  {candidates[vote_count.index(max(vote_count))]}")
 print("-----------------------------------") 
 
 # Print anaylsis table to text file
 
-# with open('financial_analy.txt', 'w') as file:
+with open('election_results.txt', 'w') as file:
    
-#     file.write("----------------------------\n")
-#     file.write("Financial Analysis"+"\n")
-#     file.write("----------------------------\n")
-#     file.write(" Total Months:  " + str(count)+"\n")
-#     file.write(" Total:  " + "$" + str(total_pl)+"\n")
-#     file.write(" Average Change:  " + str(int(ave_change))+"\n")
-#     file.write(" Greatest Increase in Profits:  " + str(incr_date) + "   $" + str(gr_increase)+"\n")
-#     file.write(" Greatest Decrease in Profits:  " + str(decr_date) + "   $" + str(gr_decrease)+"\n")
+    file.write("----------------------------\n")
+    file.write("Election Results"+"\n")
+    file.write("----------------------------\n")
+    file.write(" Total Votes:  " + str(total_votes)+"\n")
+    file.write("----------------------------\n")
+    for i in range(len(candidates)):
+       file.write(f'{candidates[i]}:  {votepercent[i]}% ({vote_count[i]})''\n')
+    file.write("----------------------------\n") 
+    file.write(f"The Winner is:  {candidates[vote_count.index(max(vote_count))]}"+"\n")
+    file.write("----------------------------\n") 
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # python-challenge - Beth's notes
 
 For the pyBank exercise, I know the average change in profits is not calculating correctly.  When I manually calculate, it comes to 2315.12.  However, when I try using the field used to capture the month to month changes I get an error.
+
+For the pyPoll exercise, I was having trouble showing 3 decimal places on the output.
